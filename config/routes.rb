e44b7ec32a4a68920bcf2de873ddad7ae74bd3fc
PharosKCL <greg.verghese@gmail.com>
2025-05-16 13:51:45 +0100

    move file structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get 'home/index'
   # You can add your other routes here if necessary
   # For static index.html in the public folder, no root route is needed
 end
